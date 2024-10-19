9a215ecf02affeec51e0f76209663ae99c0551af
wagnerk78 <wagnerkroiss@gmail.com>
2024-10-19 16:42:15 -0300

    Atualizando o JWT - parte 3

--- src/main/java/com/wagner/kroiss/core/AuthUser.java
+++ src/main/java/com/wagner/kroiss/core/AuthUser.java
@@ -1,9 +1,11 @@
 package com.wagner.kroiss.core;
 
 import com.wagner.kroiss.domain.Usuario;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import lombok.Getter;
 
+import java.util.Collection;
 import java.util.Collections;
 
 @Getter
@@ -14,8 +16,8 @@ public class AuthUser extends User {
     private Long userId;
     private String fullName;
 
-    public AuthUser(Usuario usuario) {
-        super(usuario.getEmail(), usuario.getSenha(), Collections.emptyList());
+    public AuthUser(Usuario usuario, Collection<? extends GrantedAuthority> authorities) {
+        super(usuario.getEmail(), usuario.getSenha(), authorities);
 
         this.userId = usuario.getId();
         this.fullName = usuario.getNome();
